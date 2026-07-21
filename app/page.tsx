088b79eeb2eb94d73b5b3e7738cedca070abda45
Alex Chen <elkhouri1@gmail.com>
2026-07-21 22:47:09 +0800

    error state

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
+"use client"
+
 import Container from '@mui/material/Container';
+import Alert from '@mui/material/Alert';
 import StockIndicator from '../components/StockIndicator';
 import ChartBox from '../components/ChartBox';
 import TableBox from '../components/TableBox';
 import TableDisclaimer from '@/components/TableDisclaimer';
+import { useStockStore } from '../stores/stock';
 
 export default function Home() {
+  const error = useStockStore((state) => state.error);
+  const setError = useStockStore((state) => state.setError);
+
   return (
     <Container maxWidth="md">
+      { error && <Alert className="mt-4" severity="error" onClose={() => {setError('')}}>{ error }</Alert> }
       <StockIndicator />
       <ChartBox />
       <TableBox />
