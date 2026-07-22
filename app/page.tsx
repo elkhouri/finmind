bba5fbf85dfec7390257990e91566f446ccbeefe
Alex Chen <elkhouri1@gmail.com>
2026-07-22 10:52:19 +0800

    use error bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,15 @@
-"use client"
-
 import Container from '@mui/material/Container';
-import Alert from '@mui/material/Alert';
 import StockBox from '../components/StockBox';
 import ChartBox from '../components/ChartBox';
 import TableBox from '../components/TableBox';
 import TableDisclaimer from '@/components/TableDisclaimer';
-import { useStockStore } from '../stores/stock';
+import ErrorBar from '@/components/ErrorBar';
 
 export default function Home() {
-  const error = useStockStore((state) => state.error);
-  const setError = useStockStore((state) => state.setError);
 
   return (
     <Container maxWidth="md">
-      { error && <Alert className="mt-4" severity="error" onClose={() => {setError('')}}>{ error }</Alert> }
+      <ErrorBar />
       <StockBox />
       <ChartBox />
       <TableBox />
---
+++ components/ErrorBar.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
+import { useStockStore } from '../stores/stock';
+
+export default function ErrorBar() {
+  const error = useStockStore((state) => state.error);
+  const setError = useStockStore((state) => state.setError);
+
+  function handleClose() {
+    setError('')
+  }
+  
+  return (
+    <Snackbar
+      open={!!error}
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      autoHideDuration={3000}
+      onClose={handleClose}
+    >
+      <Alert severity="error" onClose={handleClose}>{ error }</Alert>
+    </Snackbar>
+  )
+}
