334e7c4375b870ddbef69ded36e1f597a2bf7a69
Alex Chen <elkhouri1@gmail.com>
2026-07-22 13:55:39 +0800

    stock select component

---
+++ components/StockSelect.tsx
@@ -0,0 +1,97 @@
+'use client';
+
+import { TextField, Autocomplete} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import Skeleton from '@mui/material/Skeleton';
+import Stack from '@mui/material/Stack';
+import { useState, useEffect, useMemo } from 'react';
+import { getStockInfo, getStockRevenue } from '../lib/data';
+import { type Stock, useStockStore } from '../stores/stock';
+
+export default function StockSelect() {
+  const [stockInfo, setStockInfo] = useState<Stock[]>([]);
+  const [stockInfoLoading, setStockInfoLoading] = useState(false);
+  const { setCurrentStock, currentStock, setCurrentRevenue, yearPeriod, setIsLoading, setError } = useStockStore()
+
+  // fetch stock info once since it doesn't change much
+  useEffect(() => {
+    const fetchStockInfo = async () => {
+      setError('');
+      try {
+        setStockInfoLoading(true);
+        const data = await getStockInfo();
+        setStockInfo(data);
+      } catch (e) {
+        setError(e instanceof Error ? e.message : "An error occurred");
+        setStockInfo([]);
+      }
+      setStockInfoLoading(false);
+    };
+
+    fetchStockInfo();
+  }, []);
+
+  // fetch stock revenue when the user selects stock or year period, with debounce
+  useEffect(() => {
+    if (!currentStock || !currentStock.stock_id) return;
+
+    let isValid = true;
+    setIsLoading(true);
+
+    const debounceTimer = setTimeout(async () => {
+      try {
+        setError('');
+        
+        const data = await getStockRevenue(currentStock.stock_id, yearPeriod);
+        
+        if (isValid) {
+          setCurrentRevenue(data);
+        }
+      } catch (err) {
+        if (isValid) {
+          setError(err instanceof Error ? err.message : "An error occurred");
+          setCurrentRevenue([]);
+        }
+      } finally {
+        if (isValid) {
+          setIsLoading(false);
+        }
+      }
+    }, 1000);
+
+    return () => {
+      clearTimeout(debounceTimer);
+      isValid = false;
+    };
+  }, [currentStock, yearPeriod]);
+
+  // stock options that filters uniques by stock id since there's duplicates across industries etc.
+  const stockOptions = useMemo(() => {
+    return stockInfo
+      .filter((value, index, self) => {
+        return (
+          self.findIndex(item => item.stock_id === value.stock_id) === index
+        );
+      })
+  }, [stockInfo]);
+  
+  return (
+    <Autocomplete
+      disablePortal
+      disableClearable
+      options={stockOptions}
+      getOptionKey={(option) => option.stock_id + option.industry_category + option.type}
+      getOptionLabel={(option) => `${option.stock_name} (${option.stock_id})`}
+      sx={{ width: 400 }}
+      size="small"
+      popupIcon={<SearchIcon />}
+      renderInput={(params) => <TextField {...params} placeholder="輸入台／美股代號，查看公司價值" />}
+      onChange={(event, newValue) => {
+        setCurrentStock(newValue);
+        setCurrentRevenue([]);
+      }}
+      loading={stockInfoLoading}
+      loadingText={<Stack>{ Array.from({ length: 5 }, (_, i) => <Skeleton key={i} height={40}/>) }</Stack>}
+    />
+  )
+}
--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,106 +1,16 @@
-'use client';
-
-import { useState, useEffect, useMemo } from 'react';
 import {
   AppBar,
   Toolbar,
-  TextField,
   Box,
-  Autocomplete
 } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import Skeleton from '@mui/material/Skeleton';
-import Stack from '@mui/material/Stack';
-import { getStockInfo, getStockRevenue } from '../lib/data';
-import { type Stock, useStockStore } from '../stores/stock';
+import StockSelect from './StockSelect';
 
 export default function TopBar() {
-  const [stockInfo, setStockInfo] = useState<Stock[]>([]);
-  const [stockInfoLoading, setStockInfoLoading] = useState(false);
-  const { setCurrentStock, currentStock, setCurrentRevenue, yearPeriod, isLoading, setIsLoading, error, setError } = useStockStore()
-
-  // fetch stock info once since it doesn't change much
-  useEffect(() => {
-    const fetchStockInfo = async () => {
-      setError('');
-      try {
-        setStockInfoLoading(true);
-        const data = await getStockInfo();
-        setStockInfo(data);
-      } catch (e) {
-        setError(e instanceof Error ? e.message : "An error occurred");
-        setStockInfo([]);
-      }
-      setStockInfoLoading(false);
-    };
-
-    fetchStockInfo();
-  }, []);
-
-  // fetch stock revenue when the user selects stock or year period, with debounce
-  useEffect(() => {
-    if (!currentStock || !currentStock.stock_id) return;
-
-    let isValid = true;
-    setIsLoading(true);
-
-    const debounceTimer = setTimeout(async () => {
-      try {
-        setError('');
-        
-        const data = await getStockRevenue(currentStock.stock_id, yearPeriod);
-        
-        if (isValid) {
-          setCurrentRevenue(data);
-        }
-      } catch (err) {
-        if (isValid) {
-          setError(err instanceof Error ? err.message : "An error occurred");
-          setCurrentRevenue([]);
-        }
-      } finally {
-        if (isValid) {
-          setIsLoading(false);
-        }
-      }
-    }, 1000);
-
-    return () => {
-      clearTimeout(debounceTimer);
-      isValid = false;
-    };
-  }, [currentStock, yearPeriod]);
-
-  // stock options that filters uniques by stock id since there's duplicates across industries etc.
-  const stockOptions = useMemo(() => {
-    return stockInfo
-      .filter((value, index, self) => {
-        return (
-          self.findIndex(item => item.stock_id === value.stock_id) === index
-        );
-      })
-  }, [stockInfo]);
-
   return (
     <AppBar position="static" elevation={0} className="border-b border-b-gray-300">
       <Toolbar className="bg-white py-2" variant="dense">
         <Box sx={{ flexGrow: 1 }} />
-        <Autocomplete
-          disablePortal
-          options={stockOptions}
-          getOptionKey={(option) => option.stock_id + option.industry_category + option.type}
-          getOptionLabel={(option) => `${option.stock_name} (${option.stock_id})`}
-          sx={{ width: 400 }}
-          size="small"
-          popupIcon={<SearchIcon />}
-          renderInput={(params) => <TextField {...params} placeholder="輸入台／美股代號，查看公司價值" />}
-          onChange={(event, newValue) => {
-            setCurrentStock(newValue);
-            setCurrentRevenue([]);
-          }}
-          loading={stockInfoLoading}
-          loadingText={<Stack>{ Array.from({ length: 5 }, (_, i) => <Skeleton key={i} height={40}/>) }</Stack>}
-        />
+        <StockSelect />
         <Box sx={{ flexGrow: 1 }} />
       </Toolbar>
     </AppBar>
